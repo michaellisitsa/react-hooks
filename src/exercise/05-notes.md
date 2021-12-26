14cb31e6c0d1b1858b01982c3054c54e84769efe
Michael Lisitsa <michaellisitsa@Michaels-MacBook-Pro-2.local>
2021-12-26 20:59:30 +1100

    Complete React Hooks useRef challenge

---
+++ src/exercise/05-notes.md
@@ -0,0 +1,6 @@
+# Notes:
+
+- Understanding Refs -
+  https://beta.reactjs.org/learn/manipulating-the-dom-with-refs
+
+-
---
+++ src/homework/src/App.jsx
@@ -0,0 +1,35 @@
+import logo from './logo.svg'
+import './App.css'
+import {useRef, useEffect} from 'react'
+import VanillaTilt from 'vanilla-tilt'
+
+function App() {
+  const tiltBox = useRef(null)
+  console.log(tiltBox.current)
+
+  useEffect(() => {
+    VanillaTilt.init(tiltBox.current, {
+      reverse: true,
+      glare: true,
+      'full-page-listening': false,
+      easing: 'cubic-bezier(.03,.98,.52,.99)',
+      scale: '1.1',
+      speed: 200,
+      perspective: 100,
+    })
+    tiltBox.current.addEventListener('tiltChange', () => {
+      console.log('tilt changed')
+    })
+    return () => {
+      tiltBox.current.VanillaTilt.destroy()
+    }
+  }, [tiltBox])
+
+  return (
+    <div className="App">
+      <div className="red-box" ref={tiltBox}></div>
+    </div>
+  )
+}
+
+export default App
